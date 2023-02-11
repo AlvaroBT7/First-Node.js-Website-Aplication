9024ae9b9a24aebda1d4270da24df7a9e58775bd
AlvaroBT7 <123254816+AlvaroBT7@users.noreply.github.com>
2023-02-11 20:46:10 +0100

    Update indexRouter.js

--- src/routers/indexRouter.js
+++ src/routers/indexRouter.js
@@ -4,15 +4,15 @@ const router = Router();
 // routing
 
 router.get("/", (req, res) => res.render("index.ejs", {
-    // this javaScript object is carry up to the ejs files to render
+    // this javaScript object is carried up to the ejs files to render
     title: "Index"
 }))
 router.get("/about", (req, res) => res.render("about.ejs", {
-    // this javaScript object is carry up to the ejs files to render
+    // this javaScript object is carried up to the ejs files to render
     title: "About"
 }))
 router.get("/contact", (req, res) => res.render("contact.ejs", {
-    // this javaScript object is carry up to the ejs files to render
+    // this javaScript object is carried up to the ejs files to render
     title: "Contact"
 }))
 
